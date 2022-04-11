3855f652ad997e73580310d92d7ab14c9ea6852c
Gian López <85188124+netgian@users.noreply.github.com>
2022-04-11 02:09:28 +0200

    Create clock.py

---
+++ clock.py
@@ -0,0 +1,26 @@
+import requests
+import time
+
+
+token = "CHANGE ME"
+headers = {"authorization": token, "Content-Type": "application/json"}
+url = "https://discord.com/api/users/@me/settings"
+clocks = "🕛🕧🕐🕜🕑🕝🕒🕞🕓🕟🕔🕠🕕🕡🕖🕢🕗🕣🕘🕤🕙🕥🕚🕦"
+
+
+while True:
+	hour = int(time.strftime("%H"))
+	mins = int(time.strftime("%M"))
+
+	index = ((hour - 12)*2 if hour >= 12 else hour*2) + (1 if mins >= 30 else 0)
+	clock = clocks[index]
+
+	payload = {
+		"custom_status": {
+			"text": time.strftime("%H:%M:%S"),
+			"emoji_name": clock
+		}
+	}
+
+	requests.patch(url, headers=headers, json=payload)
+	time.sleep(3)
