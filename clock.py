240d40f70033b93af17fc43394c536aebf682c22
Gian Glen <85188124+netgian@users.noreply.github.com>
2024-03-06 00:02:37 +0100

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,27 +1,29 @@
-import requests
+from requests import Session
 import time
 
+TOKEN = "Account-Token"
 
+def main(token):
+    url = 'https://discord.com/api/users/@me/settings'
+    headers = {'authorization': TOKEN, 'Content-Type': 'application/json'}
+    emojis = '🕛🕧🕐🕜🕑🕝🕒🕞🕓🕟🕔🕠🕕🕡🕖🕢🕗🕣🕘🕤🕙🕥🕚🕦'
+    s = Session(headers=headers)
+    
+    while True:
+    	hour = int(time.strftime('%H'))
+    	mins = int(time.strftime('%M'))
+    
+    	emoji = emojis[((hour - 12)*2 if hour >= 12 else hour*2) + (1 if mins >= 30 else 0)]
+    
+    	payload = {
+    		'custom_status': {
+    			'text': time.strftime('%H:%M:%S'),
+    			'emoji_name': emoji
+    		}
+    	}
+    
+    	s.patch(url, json=payload)
+    	time.sleep(3)
 
-TOKEN = 'Put your token here'
-url = 'https://discord.com/api/users/@me/settings'
-headers = {'authorization': TOKEN, 'Content-Type': 'application/json'}
-clocks = '🕛🕧🕐🕜🕑🕝🕒🕞🕓🕟🕔🕠🕕🕡🕖🕢🕗🕣🕘🕤🕙🕥🕚🕦'
-
-
-while True:
-	hour = int(time.strftime('%H'))
-	mins = int(time.strftime('%M'))
-
-	index = ((hour - 12)*2 if hour >= 12 else hour*2) + (1 if mins >= 30 else 0)
-	clock = clocks[index]
-
-	payload = {
-		'custom_status': {
-			'text': time.strftime('%H:%M:%S'),
-			'emoji_name': clock
-		}
-	}
-
-	requests.patch(url, headers=headers, json=payload)
-	time.sleep(3)
+if __name__ == "__main__":
+    main(TOKEN)
